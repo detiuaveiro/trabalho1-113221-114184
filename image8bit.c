df335c7839bd0f8dd744d6ee1f49938f54f2c2b4
TiagoRG <tiago.rgarcia@ua.pt>
2023-11-16 14:51:55 +0000

    Implement ImageValidRect
    Co-authored-by: diogotavc <diogo.tav.carvalho@ua.pt>
    Signed-off-by: TiagoRG <tiago.rgarcia@ua.pt>

--- image8bit.c
+++ image8bit.c
@@ -349,6 +349,7 @@ int ImageValidPos(Image img, int x, int y) { ///
 int ImageValidRect(Image img, int x, int y, int w, int h) { ///
     assert(img != NULL);
     // Insert your code here!
+    return x >= 0 && y >= 0 && x + w <= img->width && y + h <= img->height;
 }
 
 /// Pixel get & set operations
