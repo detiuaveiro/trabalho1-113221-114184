09933e507ec336fab0ad10c6def3d7a1487ff62e
diogotavc <diogotavc@protonmail.com>
2023-11-26 14:30:46 +0000

    Test for best and worst case scenario
    Co-authored-by: TiagoRG <tiago.rgarcia@ua.pt>
    Signed-off-by: diogotavc <diogo.tav.carvalho@ua.pt>

--- imageLocateMatchBenchmarking.c
+++ imageLocateMatchBenchmarking.c
@@ -8,7 +8,7 @@
 
 void runTests() {
     FILE *file = fopen("./locate/results.csv", "w");
-    fprintf(file, "Size,Locate,Match,Fail\n");
+    fprintf(file, "Size,Best,Worst\n");
 
     for (int i = 1; i <= NUM_TESTS; i++) {
         char subimagePath[32], imagePath[32], not_subimagePath[32];
@@ -19,8 +19,8 @@ void runTests() {
         sprintf(not_subimagePath, "./locate/%d/not_subimage.pgm", i);
 
         // Load the images
-        Image subimage = ImageLoad(subimagePath);
         Image image = ImageLoad(imagePath);
+        Image subimage = ImageLoad(subimagePath);
         Image not_subimage = ImageLoad(not_subimagePath);
         assert(subimage != NULL);
         assert(image != NULL);
@@ -29,35 +29,21 @@ void runTests() {
 
         printf("[%d00%%]\n", i);
 
-        // Test ImageLocateSubImage for a match
+        // Best case scenario
         clock_t start = clock();
         int result = ImageLocateSubImage(image, &x, &y, subimage);
         clock_t end = clock();
-        double time_taken_locate = ((double)end - start) / CLOCKS_PER_SEC;
-        printf("%f\n", time_taken_locate);
+        double time_taken_best = ((double)end - start) / CLOCKS_PER_SEC;
+        printf("%f\n", time_taken_best);
 
-        // Test ImageMatchSubImage for a match
-        start = clock();
-        result = ImageMatchSubImage(image, x, y, subimage);
-        end = clock();
-        double time_taken_match = ((double)end - start) / CLOCKS_PER_SEC;
-        printf("%f\n\n", time_taken_match);
-
-        // Test ImageLocateSubImage for a fail
+        // Worst case scenario
         start = clock();
         result = ImageLocateSubImage(image, &x, &y, not_subimage);
         end = clock();
-        double time_taken_fail = ((double)end - start) / CLOCKS_PER_SEC;
-        printf("%f\n", time_taken_fail);
+        double time_taken_worst = ((double)end - start) / CLOCKS_PER_SEC;
+        printf("%f\n", time_taken_worst);
 
-        fprintf(file, "%d00%%,%f,%f,%f\n", i, time_taken_locate, time_taken_match, time_taken_fail);
-
-        // // Test ImageMatchSubImage for a fail
-        // start = clock();
-        // result = ImageMatchSubImage(image, x, y, not_subimage);
-        // end = clock();
-        // time_taken = ((double)end - start) / CLOCKS_PER_SEC;
-        // printf("%f\n\n", time_taken);
+        fprintf(file, "%d00%%,%f,%f,%f\n", i, time_taken_best, time_taken_worst);
     }
 }
 
